7150bdae4c863643d1fae7be0c25cf409c2f9497
Youi-4 <nra64321@gmail.com>
2026-03-07 12:29:46 +1100

    added more sentences

--- src/data/sentence.js
+++ src/data/sentence.js
@@ -20,8 +20,19 @@ export const paragraphs = [
     
     "An aunt is a bassoon from the right perspective. As far as we can estimate, some posit the melic myanmar to be less than kutcha. One cannot separate foods from blowzy bows. The scampish closet reveals itself as a sclerous llama to those who look. ",
 
-    "some posit the melic myanmar to be less than kutcha. One cannot separate foods from blowzy bows. The scampish closet reveals"
+    "Some posit the melic myanmar to be less than kutcha. One cannot separate foods from blowzy bows. The scampish closet reveals",
 
+    "There was some point as he grew up that he played with each of his friends for the very last time, and he had no idea that it would be the last.",
+
+    "She counted. One. She could hear the steps coming closer. Two. Puffs of breath could be seen coming from his mouth. Three. He stopped beside her. Four. She pulled the trigger of the gun.",
+
+    " It was his baby and he spent hours caring for it, pampering it, and fondling over it. She knew this all too well, and that's exactly why she had taken a sludge hammer to it.",
+    
+    "The red glint of paint sparkled under the sun. He had dreamed of owning this car since he was ten, and that dream had become a reality less than a year ago.",
+
+    "The leather jacked showed the scars of being his favorite for years. It wore those scars with pride, feeling that they enhanced his presence rather than diminishing it.",
+
+    "His parents continued to question him. He didn't know what to say to them since they refused to believe the truth. He explained again and again, and they dismissed his explanation as a figment of his imagination."
     
 ];
 export const paragraphWordMeans = (paragraph) =>{
