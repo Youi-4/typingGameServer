bcd42023def3b2736cc6524bb7bc445900316795
Youi-4 <nra64321@gmail.com>
2026-04-06 11:56:17 +1000

    added more passages for users to type

--- src/data/sentence.js
+++ src/data/sentence.js
@@ -21,6 +21,77 @@ export const paragraphs = [
 
     "The leather jacked showed the scars of being his favorite for years. It wore those scars with pride, feeling that they enhanced his presence rather than diminishing it.",
 
-    "His parents continued to question him. He didn't know what to say to them since they refused to believe the truth. He explained again and again, and they dismissed his explanation as a figment of his imagination."
+    "His parents continued to question him. He didn't know what to say to them since they refused to believe the truth. He explained again and again, and they dismissed his explanation as a figment of his imagination.",
+
+    "She needed glasses. It wasn't that she couldn't see without them, but what she could see with them. When she wore glasses, her eyes focused so deeply that she could see not only the physical but also beyond.",
+
+    "It was always the Monday mornings. It never seemed to happen on Tuesday morning, Wednesday morning, or any other morning during the week. But it happened every Monday morning like clockwork.",
+
+    "It's always good to bring a slower friend with you on a hike. If you happen to come across bears, the whole group doesn't have to worry. Only the slowest in the group do.",
+
+    "The house was located at the top of the hill at the end of a winding road. It wasn't obvious that the house was there, but everyone in town knew that it existed.",
+
+    "There was a time and a place for Stephanie to use her magic. The problem was that she had a difficult time determining this. She wished she could simply use it when the desire hit and there wouldn't be any unforeseen consequences.",
+
+    "There was a time when he would have embraced the change that was coming. In his youth, he sought adventure and the unknown, but that had been years ago.",
+
+    "It was a good idea. At least, they all thought it was a good idea at the time. Hindsight would reveal that in reality, it was an unbelievably terrible idea, but it would take another week for them to understand that.",
+
+    "She wanted rainbow hair. That's what she told the hairdresser. It should be deep rainbow colors, too. She wasn't interested in pastel rainbow hair.",
+
+    "They had always called it the green river. It made sense. The river was green. The river likely had a different official name, but to everyone in town, it was and had always been the green river.",
+
+    "After hunting for several hours, we finally saw a large seal sunning itself on a flat rock. I took one of the wooden clubs while Larry took the longer one.",
+
+    "Huge thunderstorms had brought a deluge o rain and produced flash floods heading their way. The two hikers had no idea what was coming.",
+
+    "Her breath exited her mouth in big puffs as if she were smoking a cigarette. The morning dew had made her clothes damp and she shivered from the chill in the air.",
+
+    "All he wanted was a candy bar. It didn't seem like a difficult request to comprehend, but the clerk remained frozen and didn't seem to want to honor the request.",
+
+    "He lifted the bottle to his lips and took a sip of the drink. He had tasted this before, but he couldn't quite remember the time and place it had happened.",
+
+    "He desperately searched his mind trying to locate and remember where he had tasted this when the bicycle ran over his foot.",
     
+    "Do you think you're living an ordinary life? You are so mistaken it's difficult to even explain. The mere fact that you exist makes you extraordinary. ",
+
+    "The odds of you existing are less than winning the lottery, but here you are. Are you going to let this extraordinary opportunity pass?",
+
+    "The rain and wind abruptly stopped, but the sky still had the gray swirls of storms in the distance. Dave knew this feeling all too well. The calm before the storm.",
+
+    "The wave roared towards them with speed and violence they had not anticipated. They both turned to run but by that time it was too late.",
+
+    "He tried to move on from the thought but it was persistent. It didn't want to go away and, if he was honest with himself, he didn't want it to.",
+
+    "It was going to take at least an hour to get it under control and she was sick and tired of it. She peered into the mirror and wondered if it was worth it.",
+
+    "I'm so confused by your ridiculous meltdown that I must insist on some sort of explanation for your behavior towards me. It just doesn't make any sense.",
+
+    "There's no way that I deserved the treatment you gave me without an explanation or an apology for how out of line you have been.",
+
+    "The boxed moved. That was a problem. Peter had packed the box three hours before and there was nothing inside that should make it move.",
+
+    "The question now was whether or not Peter was going to open it up and look inside to see why it had moved. The answer to that question was obvious.",
+
+    "It all started with the computer. Had he known what was to follow, he would have never logged on that day. But the truth was there was no way to know what was about to happen.",
+
+    "So Dave pressed the start button, the computer booted up, the screen came alive, and everything Dave knew to be true no longer was.",
+
+    "It was cloudy outside but not really raining. There was a light sprinkle at most and there certainly wasn't a need for an umbrella.",
+
+    "This hadn't stopped Sarah from pulling her umbrella out and opening it. It had nothing to do with the weather or the potential rain later that day. Sarah used the umbrella to hide.",
+
+    "The fog was as thick as pea soup. This was a problem. Gary was driving but couldn't see a thing in front of him.",
+
+    "He was sure that another car would end up rear-ending him, so he continued forward despite the lack of visibility. This was an unwise move.",
+
+    "Sarah watched the whirlpool mesmerized. She couldn't take her eyes off the water swirling around and around.",
+
+    "She stuck in small twigs and leaves to watch the whirlpool catch them and then suck them down. It bothered her more than a little bit that this could also be used as a metaphor for her life.",
+
+    "The opened package of potato chips held the answer to the mystery. Both detectives looked at it but failed to realize it was the key to solve the crime.",
+
+    "She sat down with her notebook in her hand, her mind wandering to faraway places. She paused and considered all that had happened.",
+
+    "It hadn't gone as expected. When the day began she thought it was going to be a bad one, but as she sat recalling the day's events to write them down, she had to admit, it had been a rather marvelous day."
 ];
